839876e5145184d61a2d6a72f48db40439afa140
Stracey2 <94061653+Mstracey2@users.noreply.github.com>
2022-11-16 13:26:28 +0000

    Added the ability to spawn coins

---
+++ IsoArcade/Assets/Clickable.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+
+    protected Camera cam;
+    protected delegate void MyDelegate();
+    protected MyDelegate inheritedFunction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            cam = Camera.main;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if(hit.collider.gameObject == this.gameObject && inheritedFunction != null)
+                {
+                    inheritedFunction();
+                }  
+            }
+        }
+    }
+}
